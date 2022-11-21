aa5c9bd2ff635866f7dd0c150c6e7da9e9ba5d4c
Martlomas <martinalejandro7a@gmail.com>
2022-11-21 08:51:23 -0500

    Add New Flags

--- LomasNunezYandun_StackLayout/LomasNunezYandun_StackLayout/MainPage.xaml.cs
+++ LomasNunezYandun_StackLayout/LomasNunezYandun_StackLayout/MainPage.xaml.cs
@@ -2,26 +2,34 @@
 
 public partial class MainPage : ContentPage
 {
-	
 
-	public MainPage()
-	{
+
+    public MainPage()
+    {
         Title = "Vertical StackLayout demo";
 
-        StackLayout stackLayout = new StackLayout { Margin = new Thickness(20) };
+        StackLayout stackLayout = new StackLayout { Margin = new Thickness(0), BackgroundColor = Colors.Gray };
 
-        stackLayout.Add(new Label { Text = "Colores primarios" });
-        stackLayout.Add(new BoxView { Color = Colors.Red, HeightRequest = 40 });
-        stackLayout.Add(new BoxView { Color = Colors.Yellow, HeightRequest = 40 });
-        stackLayout.Add(new BoxView { Color = Colors.Blue, HeightRequest = 40 });
-        stackLayout.Add(new Label { Text = "Secondary colors" });
-        stackLayout.Add(new BoxView { Color = Colors.Green, HeightRequest = 40 });
-        stackLayout.Add(new BoxView { Color = Colors.Orange, HeightRequest = 40 });
-        stackLayout.Add(new BoxView { Color = Colors.Purple, HeightRequest = 40 });
+        stackLayout.Add(new Label { Text = "Argentina", TextColor = Colors.White });
+        stackLayout.Add(new BoxView { Color = Colors.SkyBlue, HeightRequest = 45 });
+        stackLayout.Add(new BoxView { Color = Colors.White, HeightRequest = 45 });
+        stackLayout.Add(new BoxView { Color = Colors.SkyBlue, HeightRequest = 45 });
+        stackLayout.Add(new Label { Text = "Polonia", TextColor = Colors.White });
+        stackLayout.Add(new BoxView { Color = Colors.Red, HeightRequest = 65 });
+        stackLayout.Add(new BoxView { Color = Colors.White, HeightRequest = 65 });
+
+        stackLayout.Add(new Label { Text = "Holanda", TextColor = Colors.White });
+        stackLayout.Add(new BoxView { Color = Colors.Red, HeightRequest = 45 });
+        stackLayout.Add(new BoxView { Color = Colors.White, HeightRequest = 45 });
+        stackLayout.Add(new BoxView { Color = Colors.Blue, HeightRequest = 45 });
+        stackLayout.Add(new Label { Text = "Ecuador", TextColor = Colors.White });
+        stackLayout.Add(new BoxView { Color = Colors.Yellow, HeightRequest = 65 });
+        stackLayout.Add(new BoxView { Color = Colors.Blue, HeightRequest = 45 });
+        stackLayout.Add(new BoxView { Color = Colors.Red, HeightRequest = 45 });
 
         Content = stackLayout;
     }
 
-	
+
 }
 
